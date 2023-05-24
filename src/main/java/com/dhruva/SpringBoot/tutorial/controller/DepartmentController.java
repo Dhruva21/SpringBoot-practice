afaa1bdb1595804ea53492583b70db078e3c1dd9
dhruvaindavarapu <idhruva@horizon.csueastbay.edu>
2023-05-24 00:10:23 -0700

    Basic CRUD operation implemented.

---
+++ src/main/java/com/dhruva/SpringBoot/tutorial/controller/DepartmentController.java
@@ -0,0 +1,53 @@
+package com.dhruva.SpringBoot.tutorial.controller;
+
+import com.dhruva.SpringBoot.tutorial.entity.Department;
+import com.dhruva.SpringBoot.tutorial.error.DepartmentNotFoundException;
+import com.dhruva.SpringBoot.tutorial.service.DepartmentService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DepartmentController {
+    @Autowired
+    private DepartmentService departmentService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+    @PostMapping("/departments")
+    public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside SaveDeparment of DepartmentController");
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside fetchDeparment of DepartmentController");
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDeparmentById(@PathVariable("id") Long departmentId) throws DepartmentNotFoundException {
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department deleted Successfully!!";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department){
+        return departmentService.updateDepartmentById(departmentId, department);
+    }
+
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentByName(@PathVariable("name") String departmentName){
+        return departmentService.fetchDepartmentByName(departmentName);
+
+    }
+}
---
+++ src/main/java/com/dhruva/SpringBoot/tutorial/service/DepartmentService.java
@@ -0,0 +1,20 @@
+package com.dhruva.SpringBoot.tutorial.service;
+
+import com.dhruva.SpringBoot.tutorial.entity.Department;
+import com.dhruva.SpringBoot.tutorial.error.DepartmentNotFoundException;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartmentById(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
+}
